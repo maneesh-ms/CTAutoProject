29f9b4b50390880f6bdf7d8d30a089a28c6d8414
Maneesh MS <maneesh.ms@oracle.com>
2019-01-12 14:43:05 +0530

    parameterized browser

--- src/main/java/com/maven/cleartrip/framework/AppStarter.java
+++ src/main/java/com/maven/cleartrip/framework/AppStarter.java
@@ -1,5 +1,9 @@
 package com.maven.cleartrip.framework;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
 import java.util.Arrays;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
@@ -13,16 +17,35 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.maven.cleartrip.framework.BaseTest;
+import java.util.Properties;
 
 public class AppStarter {
 
 	protected WebDriver driver = null;
 
+
 	public AppStarter() {
-		System.out.println("Starting the test in default chrome..");
+		Properties properties = new Properties();
+		File file = new File(getClass().getClassLoader().getResource("config/config.properties").getFile());
+		FileInputStream fileInput = null;
+
+		try {fileInput = new FileInputStream(file);}
+		catch (FileNotFoundException e) {e.printStackTrace();}
+
+		//load properties file
+		try {properties.load(fileInput);}
+		catch (IOException e) {e.printStackTrace();}
+
 		String chrome_driver_location = this.getClass().getClassLoader().getResource("browser_drivers/chromedriver.exe").getPath();
-		System.setProperty("webdriver.chrome.driver", chrome_driver_location);
-		driver = new ChromeDriver();
+		if (properties.getProperty("browser").equals("firefox")){
+			System.out.println("Starting the test in firefox.");
+			driver = new FirefoxDriver();
+		}
+		else if(properties.getProperty("browser").equals("chrome")) {
+			System.out.println("Starting the test in chrome.");
+			System.setProperty("webdriver.chrome.driver", chrome_driver_location);
+			driver = new ChromeDriver();
+		}
 		driver.manage().window().maximize();
 	}
 
